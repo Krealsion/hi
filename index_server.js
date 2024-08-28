39a6d7adbeb5747076b7f82e2bce4eeb8d36a727
Joshua DeMoss <krealsion@gmail.com>
2024-08-28 00:32:05 -0600

    Rollback test

--- index_server.js
+++ index_server.js
@@ -73,11 +73,11 @@ app.post('/saveName', (req, res) => {
 
 app.get('/getName', (req, res) => {
   try {
-    if (req.session.name) {
-      console.log('sending name...');
-      console.log(req.session.name);
-      res.end(JSON.stringify({name: req.session.name}));
-    }
+    // if (req.session.name) {
+    //   console.log('sending name...');
+    //   console.log(req.session.name);
+    //   res.end(JSON.stringify({name: req.session.name}));
+    // }
   } catch {
     console.log("No session token found, errored out of getName. PLEASE FIX");
   }
